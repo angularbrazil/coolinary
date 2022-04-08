d23e330462e619f467b901374734bb5df8b9cf5e
andrewarosario <andrew.arosario@gmail.com>
2022-04-08 15:48:57 -0300

    feat(ui-storybook): add stories to button component

--- libs/ui/src/lib/button/button.component.stories.ts
+++ libs/ui/src/lib/button/button.component.stories.ts
@@ -2,20 +2,38 @@ import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
 
 export default {
-  title: 'ButtonComponent',
+  title: ButtonComponent.name,
   component: ButtonComponent,
+  argTypes: {
+    type: {
+      options: ['primary', 'secondary', 'danger'],
+      control: { type: 'radio'}
+    }
+  },
   decorators: [
     moduleMetadata({
       imports: [],
+      declarations: [ButtonComponent]
     })
   ],
 } as Meta<ButtonComponent>;
 
 const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
   props: args,
+  template: `<button uiBtn [type]="type">Click me!</button>`
 });
-
 
 export const Primary = Template.bind({});
 Primary.args = {
+  type: undefined
+}
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  type: 'secondary'
+}
+
+export const Danger = Template.bind({});
+Danger.args = {
+  type: 'danger'
 }
