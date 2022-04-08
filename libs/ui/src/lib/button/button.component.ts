49b002f00ed846d88fddd6b64bc0a874510fd517
andrewarosario <andrew.arosario@gmail.com>
2022-04-08 15:48:40 -0300

    feat(ui): add input types to button component

--- libs/ui/src/lib/button/button.component.ts
+++ libs/ui/src/lib/button/button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'button[uiBtn], a[uiBtn]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
 
-  constructor() { }
+  @HostBinding('class.iu-button')
+  iuButton = true;
 
-  ngOnInit(): void {
+  @Input()
+  type: 'secondary' | 'danger' | undefined;
+
+  @HostBinding('class.iu-button--secondary')
+  get secondary(): boolean {
+    return this.type === 'secondary';
+  }
+
+  @HostBinding('class.iu-button--danger')
+  get danger(): boolean {
+    return this.type === 'danger';
   }
 
 }
